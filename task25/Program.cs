6a2019d00b12aa17e0000aae0e9a4451a347667d
Kate <Kate-pretty@ya.ru>
2022-12-07 14:40:57 +0500

    added files

--- task25/Program.cs
+++ task25/Program.cs
@@ -1,19 +1,19 @@
-﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
-// НЕ ИСПОЛЬЗОВАТЬ MATH.POW
+﻿// Задача 25: (branch task_1)
+// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
+// */
 
-Console.Clear();
 Console.Write("Введите число:");
 int n = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите степень:");
 int m = Convert.ToInt32(Console.ReadLine());
-Console.Write($"Результат");
+int i = 1;   
+int res = n;
 
-int[] array = int [int m];
-
-for (int i = 0; i < array.Length; i++)
+while(i < m)
 {
-    
-}
- Console.WriteLine($"res");
+    res = (n * res);
+    i++;
+ }
+Console.Write($"Результат: {res}");
